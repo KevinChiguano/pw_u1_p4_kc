8fc949a7832e0d530cceab7bf0880567aadf2cc7
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-31 19:25:42 -0500

    calculadora final

--- js/calduladoraDeber.js
+++ js/calduladoraDeber.js
@@ -1,70 +1,70 @@
 const ponerUno = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("1");
+    let contenido = document.getElementById('idpantalla').textContent.concat("1");
     digitarPantalla(contenido);
 }
 
 const ponerDos = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("2");
+    let contenido = document.getElementById('idpantalla').textContent.concat("2");
     digitarPantalla(contenido);
 }
 
 const ponerTres = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("3");
+    let contenido = document.getElementById('idpantalla').textContent.concat("3");
     digitarPantalla(contenido);
 }
 
 const ponerCuatro = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("4");
+    let contenido = document.getElementById('idpantalla').textContent.concat("4");
     digitarPantalla(contenido);
 }
 
 const ponerCinco = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("5");
+    let contenido = document.getElementById('idpantalla').textContent.concat("5");
     digitarPantalla(contenido);
 }
 
 const ponerSeis = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("6");
+    let contenido = document.getElementById('idpantalla').textContent.concat("6");
     digitarPantalla(contenido);
 }
 
 const ponerSiete = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("7");
+    let contenido = document.getElementById('idpantalla').textContent.concat("7");
     digitarPantalla(contenido);
 }
 
 const ponerOcho = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("8");
+    let contenido = document.getElementById('idpantalla').textContent.concat("8");
     digitarPantalla(contenido);
 }
 
 const ponerNueve = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("9");
+    let contenido = document.getElementById('idpantalla').textContent.concat("9");
     digitarPantalla(contenido);
 }
 
 const ponerCero = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("0");
+    let contenido = document.getElementById('idpantalla').textContent.concat("0");
     digitarPantalla(contenido);
 }
 
 const ponerSuma = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("+");
+    let contenido = document.getElementById('idpantalla').textContent.concat("+");
     digitarPantalla(contenido);
 }
 
 const ponerResta = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("-");
+    let contenido = document.getElementById('idpantalla').textContent.concat("-");
     digitarPantalla(contenido);
 }
 
 const ponerMulti = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("*");
+    let contenido = document.getElementById('idpantalla').textContent.concat("*");
     digitarPantalla(contenido);
 }
 
 const ponerDiv = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent.concat("/");
+    let contenido = document.getElementById('idpantalla').textContent.concat("/");
     digitarPantalla(contenido);
 }
 
@@ -73,9 +73,9 @@ const borrar = ()=>{
 }
 
 const resolver = ()=>{
-    var contenido = document.getElementById('idpantalla').textContent;
+    let contenido = document.getElementById('idpantalla').textContent;
     console.log(contenido);
-    var res = eval(contenido);
+    let res = eval(contenido);
     digitarPantalla(res);
 }
 
